6a436e777c8064b0ece522bf4611f11fd2656c18
franciscogneto <francisco_gneto@hotmail.com>
2022-05-15 15:19:40 -0300

    adding cors policy

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import job
 
 
 app = FastAPI(title='UniJobs API')
-
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 # include API routes from routers module
 app.include_router(job.router)
